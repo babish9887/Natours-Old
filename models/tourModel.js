85287acf6a37a0a35ad9555216d1387b3abecb44
babish9887 <babish9887@mgmail.com>
2024-03-10 23:38:50 +0545

    added mongodb middlewares and more

--- models/tourModel.js
+++ models/tourModel.js
@@ -13,15 +13,6 @@ const tourSchema = new mongoose.Schema(
       minlength: [10, 'A tour name must have more or equal then 10 characters']
       // validate: [validator.isAlpha, 'Tour name must only contain characters']
     },
-    slug: String,
-    duration: {
-      type: Number,
-      required: [true, 'A tour must have a duration']
-    },
-    maxGroupSize: {
-      type: Number,
-      required: [true, 'A tour must have a group size']
-    },
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
@@ -36,14 +27,6 @@ const tourSchema = new mongoose.Schema(
       min: [1, 'Rating must be above 1.0'],
       max: [5, 'Rating must be below 5.0']
     },
-    ratingsQuantity: {
-      type: Number,
-      default: 0
-    },
-    price: {
-      type: Number,
-      required: [true, 'A tour must have a price']
-    },
     priceDiscount: {
       type: Number,
       validate: {
@@ -54,6 +37,24 @@ const tourSchema = new mongoose.Schema(
         message: 'Discount price ({VALUE}) should be below regular price'
       }
     },
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration']
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size']
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price']
+    },
+   
     summary: {
       type: String,
       trim: true,
--- models/userModel.js
+++ models/userModel.js
@@ -1,113 +1,36 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-const bc = require('bcryptjs');
-const crypto = require('crypto');
+const mongoose=require('mongoose');
+const validator=require('validator');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Please tell us your name']
-  },
-  email: {
-    type: String,
-    required: [true, 'Please provide your email'],
-    unique: true,
-    lowercase: true,
-    validate: [validator.isEmail, 'Please provide a valid email']
-  },
-  photo: String,
-  role: {
-    type: String,
-    enum: ['user', 'guide', 'lead-guide', 'admin'],
-    default: 'user'
-  },
-  password: {
-    type: String,
-    required: [true, 'Please provide a password'],
-    validate: {
-      validator: function(v) {
-        return v.length >= 8;
-      },
-      message: 'Please provide a valid email address'
+
+const userSchema=new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, 'Please tell us your name']
     },
-    select: false
-  },
-  passwordConfirm: {
-    type: String,
-    required: [true, ' Please confirm you password'],
-    validate: {
-      validator: function(el) {
-        return el === this.password;
-      },
-      message: 'Password does not match'
+    email: {
+        type: String,
+        required: [true, 'Please provide your email'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, "Please provide a valid email"]
+    },
+    photo: String,
+    password:{
+        type: String,
+        required: [true, 'Please provide a password'],
+        validate:{
+            validator: function(v){
+                return v.length>=8
+            },
+            message: 'Please provide a valid email address'
+        }
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true,' Please confirm you password']
     }
-  },
-  passwordChangedAt: Date,
-  passwordResetToken: String,
-  passwordResetExpire: Date,
-  active: {
-    type: Boolean,
-    default:true,
-    select: false
-  }
+
 });
 
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-
-  this.password = await bc.hash(this.password, 12);
-  this.passwordConfirm = undefined;
-  next();
-});
-
-userSchema.pre('save', function(next) {
-  if (this.isModified('password') || this.isNew) return next();
-
-  this.passwordChangedAt = Date.now() - 1000;
-  next();
-});
-
-userSchema.pre('/^find/', function(next){
-  this.find({active:{$ne: false}});
-  next();
-})
-
-
-userSchema.methods.correctPassword = async function(
-  candidatePassword,
-  usePassword
-) {
-  return await bc.compare(candidatePassword, usePassword);
-};
-
-
-
-
-userSchema.methods.changedPasswordAfter = function(JWTTimeStamp) {
-  if (this.passwordChangedAt) {
-    const changedTimestamp = parseInt(
-      this.passwordChangedAt.getTime() / 1000,
-      10
-    );
-    console.log(changedTimestamp, JWTTimeStamp);
-    return JWTTimeStamp > changedTimestamp; //100 < 200
-  }
-
-  return false;
-};
-
-userSchema.methods.createPasswordResetToken = function() {
-  const resetToken = crypto.randomBytes(32).toString('hex');
-
-  this.passwordResetToken = crypto
-    .createHash('sha256')
-    .update(resetToken)
-    .digest('hex');
-
-  console.log({ resetToken }, this.passwordResetToken);
-  this.passwordResetExpire = Date.now() + 10 * 60 * 1000;
-  return resetToken;
-};
-
-const user = mongoose.model('user', userSchema);
-module.exports = user;
+const user=mongoose.model('user', userSchema);
+module.exports=user;
